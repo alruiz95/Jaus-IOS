2b639956cf0fac99e182ba9c51a1d388e8f221bf
Estudiante <estudiante@mrsafe.local>
2016-09-27 13:51:03 -0600

    Initial Commit

---
+++ ProyectoJaus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProyectoJaus
+//
+//  Created by Estudiante on 9/27/16.
+//  Copyright © 2016 Tecnologico de Costa Rica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
